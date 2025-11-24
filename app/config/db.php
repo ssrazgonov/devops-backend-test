33ecb5bc8de643dfeb1775934ff34d7c4bf05d12
Sergey <ssrazgonov@gmail.com>
2025-11-24 04:35:09 +0300

    feat(Env): добавлены переменные окружения

--- app/config/db.php
+++ app/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=yii2',
-    'username' => 'root',
-    'password' => 'root',
+    'dsn' => 'mysql:host=' . $_ENV['MYSQL_HOST'] . ';dbname=' . $_ENV['MYSQL_DATABASE'],
+    'username' =>  $_ENV['MYSQL_USER'] ,
+    'password' =>  $_ENV['MYSQL_PASSWORD'] ,
     'charset' => 'utf8',
 ];
--- app/web/index.php
+++ app/web/index.php
@@ -7,5 +7,8 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '../../');
+$dotenv->safeLoad();
+
 $config = require __DIR__ . '/../config/config.php';
 (new yii\web\Application($config))->run();
